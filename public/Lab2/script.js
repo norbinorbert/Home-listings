aa6a01c249788eb47de1027807c9877a4015e274
norbinorbert <norbi_boda@yahoo.com>
2024-04-09 23:41:24 +0300

    generating random formulas, rendering them when game starts, clearing the canvas on restart

--- public/Lab2/script.js
+++ public/Lab2/script.js
@@ -1,16 +1,42 @@
 document.getElementById('form').addEventListener('submit', (event) => {
   const submitButton = document.getElementById('submit');
   const formElements = document.getElementsByTagName('input');
+  const canvas = document.getElementById('canvas');
+  const canvasContent = canvas.getContext('2d');
   if (submitButton.textContent == 'Start game') {
     submitButton.textContent = 'Restart';
+    let listOfOperators = [];
     for (i = 0; i < formElements.length; i++) {
       formElements[i].disabled = true;
+      if (formElements[i].type == 'checkbox' && formElements[i].checked) {
+        listOfOperators.push(document.querySelector(`label[for="${formElements[i].id}"]`).textContent);
+      }
+    }
+    const numberOfProblems = parseInt(document.getElementById('number-of-problems').value);
+    topLeftPoint = [0, 30];
+    width = 100;
+    height = 50;
+    for (i = 0; i < numberOfProblems; i++) {
+      let number1 = Math.floor(Math.random() * 100 + 1);
+      let number2 = Math.floor(Math.random() * 100 + 1);
+      let index = Math.floor(Math.random() * listOfOperators.length);
+      let operator = listOfOperators[index];
+      let answer = eval(number1 + operator + number2);
+      canvasContent.font = '20px Times New Roman';
+      canvasContent.fillText(`${number1} ${operator} ${number2}`, topLeftPoint[0], topLeftPoint[1]);
+      topLeftPoint[1] += height + 1;
     }
   } else {
     submitButton.textContent = 'Start game';
     for (i = 0; i < formElements.length; i++) {
       formElements[i].disabled = false;
+      if (formElements[i].type == 'checkbox') {
+        formElements[i].checked = false;
+      } else {
+        formElements[i].textContent = '';
+      }
     }
+    canvasContent.clearRect(0, 0, canvas.width, canvas.height);
   }
   event.preventDefault();
 });
